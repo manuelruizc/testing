f1fcdf42d65f2af0c9309a9f94bed8bad2582116
manuelruizc <manuelruizwebdev@gmail.com>
2021-11-05 09:38:25 -0600

    Fix for android

--- android/app/src/main/java/com/icentris/kwivrr/MainApplication.java
+++ android/app/src/main/java/com/icentris/kwivrr/MainApplication.java
@@ -38,7 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new R5Package());
+      // packages.add(new R5Package());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
